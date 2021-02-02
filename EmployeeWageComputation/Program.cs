4fbe41958803ab8b4505703a8dfd61e48e6a9833
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-02 15:08:01 +0530

    [MaheshKangude] ADDED: functionality to check employee attendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace EmployeeWageComputation
+namespace EmployeeWageComputationNameSpace
 {
     class EmployeeWageComputation
     {
+        private int IsEmployeePresent() {
+            return new Random().Next() % 2;
+        }
+
+        public void CheckEmployeeAttendance() { 
+            if (IsEmployeePresent() == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+
         static void Main(string[] args)
         {
+            EmployeeWageComputation employeeWageComputation = new  EmployeeWageComputation();
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            employeeWageComputation.CheckEmployeeAttendance();
         }
     }
 }
