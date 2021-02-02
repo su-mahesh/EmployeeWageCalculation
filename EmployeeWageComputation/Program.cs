0bafd38af604ed42519f0b6d72133812001f49b5
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-02 16:49:04 +0530

    [MaheshKangude] REFACTORED: fucntionality to calculate wage for a month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,28 +5,41 @@ namespace EmployeeWageComputationNameSpace
     class EmployeeWageComputation
     {
         private const float EmpWagePerHour = 20;
-        private const int FullTimeWorkingHrsPerDay = 8;
-        private const int PartTimeWorkingHrsPerDay = 4;
+        private const int FullTime_WorkingHrs_PerDay = 8;
+        private const int PartTime_WorkingHrs_PerDay = 4;
         private const int IS_FULL_TIME = 1;
         private const int IS_PART_TIME = 2;
+        private const int IS_ABSENT = 0;
+        private const int Num_WorkingDays_PerMonth = 20;
+        private float EmpMonthlyWage = 0;
+        float EmpDailyWage = 0;
 
         private int IsEmployeePresent() {
             return new Random().Next() % 3;
         }
 
-        public void CalculateDailyWage() {
-            float EmpDailyWage = 0;
+        public void CalculateWage() {
+            
+            int DayNumber = 1;
+            int EmpWorkinhHrs = 0;
+            while (DayNumber <= Num_WorkingDays_PerMonth) {
+                switch (IsEmployeePresent()) {
+                    case IS_ABSENT:
+                        EmpWorkinhHrs = 0;
+                        break;
+                    case IS_PART_TIME:
+                        EmpWorkinhHrs = PartTime_WorkingHrs_PerDay;
+                        break;
+                    case IS_FULL_TIME:
+                        EmpWorkinhHrs = FullTime_WorkingHrs_PerDay;
+                        break;
+                }
+                EmpDailyWage = EmpWorkinhHrs * EmpWagePerHour;
 
-            switch (IsEmployeePresent()) {
-                case IS_PART_TIME:
-                    EmpDailyWage = PartTimeWorkingHrsPerDay * EmpWagePerHour;
-                    break;
-                case IS_FULL_TIME:
-                    EmpDailyWage = FullTimeWorkingHrsPerDay * EmpWagePerHour;
-                    break;
+                EmpMonthlyWage += EmpDailyWage;
+                DayNumber++;
             }
-
-            Console.WriteLine("Employee Daily :"+EmpDailyWage);
+            Console.WriteLine("Employee monthly wage :"+ EmpMonthlyWage);
         }
 
         static void Main(string[] args)
@@ -34,7 +47,7 @@ namespace EmployeeWageComputationNameSpace
             EmployeeWageComputation employeeWageComputation = new  EmployeeWageComputation();
             Console.WriteLine("Welcome to Employee Wage Computation");
 
-            employeeWageComputation.CalculateDailyWage();
+            employeeWageComputation.CalculateWage();
         }
     }
 }
