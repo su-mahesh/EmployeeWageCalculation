09917874e0f7eb62383a5070a9b3adf09a606107
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-02 16:03:06 +0530

    [MaheshKangude] ADDED: partime emp wage calculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,22 +5,18 @@ namespace EmployeeWageComputationNameSpace
     class EmployeeWageComputation
     {
         private const float EmpWagePerHour = 20;
-        private const int WorkingHoursPerDay = 8;
+        private const int FullTimeWorkingHrsPerDay = 8;
+        private const int PartTimeWorkingHrsPerDay = 4;
         private int IsEmployeePresent() {
-            return new Random().Next() % 2;
-        }
-
-        public void CheckEmployeeAttendance() { 
-            if (IsEmployeePresent() == 1)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
+            return new Random().Next() % 3;
         }
 
         public void CalculateDailyWage() {
             float EmpDailyWage = 0;
             if (IsEmployeePresent() == 1)
-                EmpDailyWage = WorkingHoursPerDay * EmpWagePerHour;
+                EmpDailyWage = FullTimeWorkingHrsPerDay * EmpWagePerHour;
+            else if (IsEmployeePresent() == 2)
+                EmpDailyWage = PartTimeWorkingHrsPerDay * EmpWagePerHour;
             Console.WriteLine("Employee Daily :"+EmpDailyWage);
         }
 
