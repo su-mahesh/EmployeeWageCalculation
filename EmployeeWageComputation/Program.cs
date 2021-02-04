f07ce00cde19333064966f825b18514768c74693
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-04 12:27:07 +0530

    [MaheshKangude] REACTORED: structured using class and methods

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,19 +4,28 @@ namespace EmployeeWageComputationNameSpace
 {
     class EmployeeWageComputation
     {
-        private const float EmpWagePerHour = 20;
-        private const int FullTime_WorkingHrs_PerDay = 8;
-        private const int PartTime_WorkingHrs_PerDay = 4;
-        private const int MAX_WORKING_HRS = 100;
-        private const int MAX_WORKING_DAYS = 20;
+        private float EmpWagePerHour = 20;
+        private int FullTime_WorkingHrs_PerDay = 8;
+        private int PartTime_WorkingHrs_PerDay = 4;
+        private int MAX_WORKING_HRS = 100;
+        private int MAX_WORKING_DAYS = 20;
         private const int IS_FULL_TIME = 1;
         private const int IS_PART_TIME = 2;
         private const int IS_ABSENT = 0;
         float EmpDailyWage = 0;
         private float TotalWage = 0;
 
+        public EmployeeWageComputation(int EmpWagePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            this.EmpWagePerHour = EmpWagePerHour;
+            this.FullTime_WorkingHrs_PerDay = FullTime_WorkingHrs_PerDay;
+            this.PartTime_WorkingHrs_PerDay = PartTime_WorkingHrs_PerDay;
+            this.MAX_WORKING_HRS = MAX_WORKING_HRS;
+            this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+        }
+
         private int IsEmployeePresent() {
-            return new Random().Next() % 3;
+            return new Random().Next(0, 3);
         }
 
         public void CalculateWage() {
@@ -37,7 +46,6 @@ namespace EmployeeWageComputationNameSpace
                         break;
                 }
                 EmpDailyWage = EmpWorkinghHrs * EmpWagePerHour;
-
                 TotalWage += EmpDailyWage;
                 DayNumber++;
                 TotalWorkingHrs += EmpWorkinghHrs;
@@ -46,10 +54,9 @@ namespace EmployeeWageComputationNameSpace
         }
 
         static void Main(string[] args)
-        {
-            EmployeeWageComputation employeeWageComputation = new  EmployeeWageComputation();
+        {        
             Console.WriteLine("Welcome to Employee Wage Computation");
-
+            EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation(20, 8, 4, 100, 20);
             employeeWageComputation.CalculateWage();
         }
     }
