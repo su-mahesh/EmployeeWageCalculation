eb767ec398105339517d2f9282796bff070e0ce4
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-05 14:02:11 +0530

    [MaheshKangude] ADDED: AbilityToSave TotalWage ForEachCompany

---
+++ EmployeeWageComputation/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputationNameSpace
+{
+    class Company
+    {
+        public String CompanyName;
+        public float EmpWagePerHour = 20;
+        public int FullTime_WorkingHrs_PerDay = 8;
+        public int PartTime_WorkingHrs_PerDay = 4;
+        public int MAX_WORKING_HRS = 100;
+        public int MAX_WORKING_DAYS = 20;
+        public float TotalWage = 0;
+        
+        public Company(String CompanyName, int EmpWagePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            this.CompanyName = CompanyName;
+            this.EmpWagePerHour = EmpWagePerHour;
+            this.FullTime_WorkingHrs_PerDay = FullTime_WorkingHrs_PerDay;
+            this.PartTime_WorkingHrs_PerDay = PartTime_WorkingHrs_PerDay;
+            this.MAX_WORKING_HRS = MAX_WORKING_HRS;
+            this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -3,25 +3,6 @@ using System.Collections.Generic;
 
 namespace EmployeeWageComputationNameSpace
 {
-    class Company
-    {
-        public float EmpWagePerHour = 20;
-        public int FullTime_WorkingHrs_PerDay = 8;
-        public int PartTime_WorkingHrs_PerDay = 4;
-        public int MAX_WORKING_HRS = 100;
-        public int MAX_WORKING_DAYS = 20;
-        public String CompanyName;
-
-        public Company(String CompanyName, int EmpWagePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
-        {
-            this.CompanyName = CompanyName;
-            this.EmpWagePerHour = EmpWagePerHour;
-            this.FullTime_WorkingHrs_PerDay = FullTime_WorkingHrs_PerDay;
-            this.PartTime_WorkingHrs_PerDay = PartTime_WorkingHrs_PerDay;
-            this.MAX_WORKING_HRS = MAX_WORKING_HRS;
-            this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
-        }
-    }
     class EmployeeWageComputation
     {
         private const int IS_FULL_TIME = 1;
@@ -71,8 +52,9 @@ namespace EmployeeWageComputationNameSpace
                     DayNumber++;
                     TotalWorkingHrs += EmpWorkinghHrs;
                 }
+                company.TotalWage = TotalWage;
                 Console.WriteLine("\nCompany name: "+CompanyName);
-                Console.WriteLine("Total working days :" + (DayNumber - 1) + "\nTotal working hours :" + TotalWorkingHrs + "\nTotal employee wage :" + TotalWage);
+                Console.WriteLine("Total working days :" + (DayNumber - 1) + "\nTotal working hours :" + TotalWorkingHrs + "\nTotal employee wage :" + company.TotalWage);
 
             }
             catch (Exception e) {
@@ -89,8 +71,6 @@ namespace EmployeeWageComputationNameSpace
             employeeWageComputation.AddCompany("Mahindra", 30, 8, 4, 100, 20);
             employeeWageComputation.CalculateEmpWage("tata");
             employeeWageComputation.CalculateEmpWage("Mahindra");
-        }
-
-       
+        } 
     }
 }
